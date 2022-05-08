2fdf91000a9f4ef100a5aacf6189f367bb10de6e
padma k <101664110+paddhu27@users.noreply.github.com>
2022-05-08 22:56:57 +0530

    task 1

--- binary search.py
+++ binary search.py
@@ -1,6 +1,5 @@
 list=input("enter elements separated by spaces\n")
 arr=list.split()
-s_ele=input("enter a element that you want to find : ")
 n=len(arr)
 def bubbleSort(arr):
     for i in range(n):
@@ -8,20 +7,23 @@ def bubbleSort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
-def binary_search(arr,s_ele):
+org=bubbleSort(arr)
+print("the elements in sorted order is\n",org)
+s_ele=input("enter a element that you want to find : ")
+def binary_search(org,s_ele):
     low= 0
-    high= len(arr) - 1
+    high= len(org) - 1
     mid = 0
     while(low<=high):
         mid=(low+high)//2
-        if(arr[mid]==s_ele):
+        if(org[mid]==s_ele):
             return mid
-        elif(s_ele<arr[mid]):
+        elif(s_ele<org[mid]):
             high=mid-1
-        elif(s_ele>arr[mid]):
+        elif(s_ele>org[mid]):
             low=mid+1
     return -1
-r= binary_search(arr,s_ele)
+r= binary_search(org,s_ele)
 if r!= -1:
     print("Element is present at index", r)
 else:
